c38a23f1c08315225a40cb215033812bbff86fec
NoFishLikeIan <andreatitton96@gmail.com>
2019-08-24 20:32:04 +0200

    🎨 added colors!

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import { sync, stream, fromEvent, fromPromise } from "@thi.ng/rstream";
-import { map, comp, filter, mapcat, mapIndexed, range, zip } from "@thi.ng/transducers";
+import { map, comp, filter, mapcat, mapIndexed, range, zip, min, max } from "@thi.ng/transducers";
 import { fit } from '@thi.ng/math'
 import { svg, line } from '@thi.ng/hiccup-svg'
 import { updateDOM } from '@thi.ng/transducers-hdom'
-import { GRADIENTS, cosineGradient, rgba, rgbaCss } from '@thi.ng/color'
+import { GRADIENTS } from '@thi.ng/color'
 
 import { readCsv } from "./utils/reading/read-csv";
 import { isTruthy } from "./core";
-
 import { computeSeriesAutocor } from "./utils/time-series/autocorrelation";
 import { extractSeries } from "./utils/reading/read-data";
 import { rect } from "./components/rect";
 import { AIC } from "./utils/time-series/aic";
+import { fitToColor } from "./utils/scales";
 
 const URL = 'https://gist.githubusercontent.com/NoFishLikeIan/0c7f070b056773ca5294bb9767fcbc23/raw/996f26fc4792eb47e252d3cd10c9ecb3f0599722/melbourne.csv'
-const PERIODS = 10
+const PERIODS = 30
 
 const lags = range(PERIODS, 1)
-const aicColors = map((gradient) => rgbaCss(gradient as number[]), cosineGradient(PERIODS, GRADIENTS["orange-blue"]))
+const fitToOrangeBlue = fitToColor(GRADIENTS['cyan-magenta'])
+
 
 const error = stream<any>();
 // const datasetUrl = stream<string>();
@@ -48,7 +49,7 @@ const autocorrelation = sync<any, any>({
 const orderSelection = sync<any, any>({
   src: { series },
   xform: comp(
-    map(({ series }) => map(l => AIC(series, l), lags)),
+    map(({ series }) => [...map(l => AIC(series, l), lags)]),
   )
 })
 
@@ -61,7 +62,6 @@ const chart = sync<any, any>({
     )
   },
   xform: map(({ autocorrelation, orderSelection, window }) => {
-    console.log('Plotting', { autocorrelation, orderSelection: [...orderSelection] })
     const [width, height]: [number, number] = window
 
 
@@ -77,12 +77,19 @@ const chart = sync<any, any>({
     const mapBarHeight = (y: number) =>
       fit(y, -1, 1, by / 2, -by / 2)
 
-    const iter = zip(autocorrelation, orderSelection)
+    const mapColor = fitToOrangeBlue(min(orderSelection), max(orderSelection))
+
+
+    const iter = zip(autocorrelation, orderSelection) as Iterable<[number, number]>
 
     return svg(
       { width, height },
       line([MARGIN_X, center], [width - MARGIN_X, center], { stroke: 'black' }),
-      mapIndexed((index, corr) => rect([mapXBar(index), center], 20, mapBarHeight(corr)), autocorrelation)
+      mapIndexed(
+        (index, [corr, order]: [number, number]) =>
+          rect([mapXBar(index), center], 20, mapBarHeight(corr), { fill: mapColor(order) }),
+        iter
+      )
     )
 
   })
---
+++ src/utils/scales.ts
@@ -0,0 +1,17 @@
+import { cosineColor, rgbaCss, ReadonlyColor, cosineCoeffs, CosGradientSpec, GRADIENTS } from "@thi.ng/color"
+import { fit } from "@thi.ng/math";
+
+type ColorScalingFn = (min: number, max: number) => (y: number) => string
+
+export function fitToColor(spec: CosGradientSpec): ColorScalingFn
+export function fitToColor(spec: ReadonlyColor, to: ReadonlyColor): ColorScalingFn
+export function fitToColor(spec?: any, to?: any): ColorScalingFn {
+    return (min: number, max: number) =>
+        (y: number) =>
+            spec
+                ? to
+                    ? rgbaCss(cosineColor(cosineCoeffs(spec, to), fit(y, min, max, 0, 1)))
+                    : rgbaCss(cosineColor(spec, fit(y, min, max, 0, 1)))
+
+                : fitToColor(GRADIENTS['cyan-magenta'])(min, max)(y)
+}
